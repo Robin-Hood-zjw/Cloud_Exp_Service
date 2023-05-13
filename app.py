0ae23cfe7cadb3a2484ae9930c8530becbf8e055
Robin-Hood-zjw <jiawenz@umich.edu>
2023-05-13 02:41:54 -0400

    build 1st post request endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -16,4 +16,11 @@ stores = [
 
 @app.get("/store")
 def get_stores():
- return {"stores": stores}
+    return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+   request_data = request.get_json()
+   new_store = {"name": request_data["name"], "items": []}
+   stores.append(new_store)
+   return new_store, 201
