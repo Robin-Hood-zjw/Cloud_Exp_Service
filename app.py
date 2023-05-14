f6b918dae1c3bde48c88ad316a5877c4db2600b3
Robin-Hood-zjw <jiawenz@umich.edu>
2023-05-13 22:00:45 -0400

    improve endpoints 3

--- app.py
+++ app.py
@@ -6,6 +6,46 @@ from db import items, stores
 
 app = Flask(__name__)
 
+# Store Part
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+@app.post("/store")
+def create_store():
+   store_data = request.get_json()
+
+   if "name" not in store_data:
+       abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+   for store in stores.values():
+       if store_data["name"] == store["name"]:
+           abort(400, message="Store already exists.")
+
+   store_id = uuid.uuid4().hex
+   store = {**store_data, "id": store_id}
+   stores[store_id] = store
+   return store, 201
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except:
+        abort(404, message="Store not found.")
+
+
+# Item Part
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
@@ -34,24 +74,6 @@ def create_item():
 
     return item, 201
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
-
-@app.post("/store")
-def create_store():
-   store_data = request.get_json()
-   if "name" not in store_data:
-       abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
-   for store in stores.values():
-       if store_data["name"] == store["name"]:
-           abort(400, message="Store already exists.")
-
-   store_id = uuid.uuid4().hex
-   store = {**store_data, "id": store_id}
-   stores[store_id] = store
-   return store, 201
-
 @app.put("/item/<string:item_id>")
 def update_item(item_id):
     item_data = request.get_json()
@@ -64,26 +86,6 @@ def update_item(item_id):
         return item
     except KeyError:
         abort(404, message="Item not found.")
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "Store deleted."}
-    except:
-        abort(404, message="Store not found.")
 
 @app.delete("/item/<string:item_id>")
 def delete_item(item_id):
