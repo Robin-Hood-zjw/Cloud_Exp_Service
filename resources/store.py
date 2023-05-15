8e8519e008c852d14d8a7614e6ca5706b518f0e7
Robin-Hood-zjw <jiawenz@umich.edu>
2023-05-14 21:43:02 -0400

    add a endpoint for store blueprint

--- resources/store.py
+++ resources/store.py
@@ -20,3 +20,23 @@ class Store(MethodView):
             return {"message": "Store deleted."}
         except:
             abort(404, message="Store not found.")
+
+@blp.route("/store")
+class StoreList(MethodView):
+    def get(self):
+       return {"stores": list(stores.values())}
+
+    def post(self):
+        store_data = request.get_json()
+
+        if "name" not in store_data:
+            abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+        
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message="Store already exists.")
+                
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+        return store, 201
