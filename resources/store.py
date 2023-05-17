d1426157fdf60cc72ac9fc16b6d800782f7fa367
Robin-Hood-zjw <jiawenz@umich.edu>
2023-05-17 03:09:15 -0400

    advance db settings

--- resources/store.py
+++ resources/store.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
-from db import stores
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 
@@ -36,6 +38,19 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
+        store = StoreModel(**store_data)
+
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="A error occurred creating the store.")
+
+
+
+
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message="Store already exists.")
