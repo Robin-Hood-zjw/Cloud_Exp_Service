c343aa7508e7db3d4d8b37199368e6a37037c69f
Robin-Hood-zjw <jiawenz@umich.edu>
2023-06-01 01:27:18 -0400

    build the initial tag schemas and models

---
+++ resources/tag.py
@@ -0,0 +1,22 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import TagModel, StoreModel
+from schemas import TagSchema
+
+blp = Blueprint("Tags", "tags", description="Operations on tags")
+
+
+@blp.route("/tag/<string:store_id>/tag")
+class TagInStore(MethodView):
+    @blp.response(200, TagSchema(many=True))
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store.tags.all()
+
+    @blp.arguments(TagSchema)
+    @blp.response(201, TagSchema)
+    def post(self, tag_data, store_id):
+        tag = TagModel(**tag_data, store_id=store_id)
--- schemas.py
+++ schemas.py
@@ -9,13 +9,23 @@ class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Str()
+    store_id = fields.Int()
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)
+    store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
