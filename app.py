eff57ab67a839af43f8e62176bb3974478b874ca
Robin-Hood-zjw <jiawenz@umich.edu>
2023-06-02 02:22:41 -0400

    build intro to jwt

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+
 from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
@@ -19,12 +22,15 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     
     db.init_app(app)
     api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
