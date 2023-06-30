24b5a96db469dcaa2d689802a01e4055c7e8fbd2
Robin-Hood-zjw <jiawenz@umich.edu>
2023-06-30 16:07:59 -0400

    build the login endpoint

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,8 @@
 from flask.views import MethodView
-from flask_smorest import Blueprint, abort
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from passlib.hash import pbkdf2_sha256
+from flask_smorest import Blueprint, abort
+from flask_jwt_extended import create_access_token
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from db import db
 from models import UserModel
@@ -10,8 +11,9 @@ from schemas import UserSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
+
 @blp.route('/register')
-class USerRegister(MethodView):
+class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
         if UserModel.query.filter(UserModel.username == user_data["username"]).first():
@@ -28,3 +30,16 @@ class USerRegister(MethodView):
         db.session.delete(user)
         db.session.commit()
         return {"message": "User deleted."}, 200
+    
+
+@blp.route('/login')
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+        
+        abort(401, message="Invalid credentials.")
