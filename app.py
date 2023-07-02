374ed41471aa8dc35884d2933b8b11560f937347
Robin-Hood-zjw <jiawenz@umich.edu>
2023-07-02 15:22:40 -0400

    error handling when a fresh token is required

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ def create_app(db_url=None):
     def invalid_token_callback(error):
         return jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401
     
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_paylod):
+        return jsonify({"description": "The token is not fresh", "error": "fresh_token_required"}), 401
+    
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return jsonify({"description": "Request does contain an access token.", "error": "authorization_required"}), 401
